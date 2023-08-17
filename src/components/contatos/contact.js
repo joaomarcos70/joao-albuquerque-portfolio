a9d0aeb8cdd8427a7d27a3e25f4c3f757159f2ad
João Albuquerque <joaomarcos70@outlook.com.br>
2023-08-17 02:00:38 -0300

    feat: corrigindo componente de carrousel

--- src/components/contatos/contact.js
+++ src/components/contatos/contact.js
@@ -4,6 +4,10 @@ import IconComponent from "../../shared/icons/iconComponent";
 import SvgIcons from "../../shared/icons/iconSvg";
 
 const Contact = () => {
+
+  const openLink = (url) =>{
+    window.open(url)
+  }
   return (
     <>
       <section className="contact-area">
@@ -11,10 +15,10 @@ const Contact = () => {
           joaomarcos70@outlook.com.br
         </div>
         <div className="social">
-          <div className="linkedin">
+          <div className="linkedin" onClick={()=> openLink('https://www.linkedin.com/in/joaoalbuquerq/')}>
               <SvgIcons name="linkedin" color={"#343E46"} classes={"iconSocial"}></SvgIcons>
           </div>
-          <div className="github">
+          <div className="github" onClick={()=> openLink('https://github.com/joaomarcos70')}>
               <SvgIcons name="github" color={"#343E46"} classes={"iconSocial"}></SvgIcons>
           </div>
         </div>
--- src/components/projetos/projetos.js
+++ src/components/projetos/projetos.js
@@ -1,27 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import "./projetos.scss";
 import IconComponent from "../../shared/icons/iconComponent";
+import itau from "../../shared/images/projects/itau.jpg";
+import omnik from "../../shared/images/projects/omnik.jpg";
+import fcamara from "../../shared/images/projects/fcamara.jpg";
 
 const Projetos = () => {
-  return (
-    <>
-      <section className="container">
-        <div className="display">
-          <IconComponent icon="chevron-right" className="chevron" />
-          <div className="line"></div>
-          <IconComponent icon="chevron-left" className="chevron" />
-        </div>
+  const carrouselData = [
+    {
+      order: 1,
+      src: itau,
+      title: "itau",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    },
+    {
+      order: 2,
+      src: omnik,
+      title: "omnik",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    },
+    {
+      order: 3,
+      src: fcamara,
+      title: "fcamara",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    },
+  ];
 
-        <section className="container-carrousel">
-          <div className="carrousel">
-            <h1>teste</h1>
-          </div>
-          <div className="description">
-            <p>teste</p>
-          </div>
-        </section>
-      </section>
-    </>
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const next = () => {
+    setCurrentSlide((prevSlide) => (prevSlide + 1) % carrouselData.length);
+  };
+  const before = () => {
+    setCurrentSlide((prevSlide) =>
+      prevSlide === 0 ? carrouselData.length - 1 : prevSlide - 1
+    );
+  };
+
+  return (
+    <section className="container">
+      <div className="display">
+        <div className="icon" onClick={next}>
+        <IconComponent
+          icon="chevron-right"
+          className="chevron"
+        />
+        </div>
+        <div className="line"></div>
+        <div className="icon" onClick={before}>
+        <IconComponent icon="chevron-left" className="chevron" />
+        </div>
+      </div>
+
+      {carrouselData.map((item, index) =>
+        index === currentSlide ? (
+          <section className="container-carrousel" key={item.title}>
+            <div className="carrousel">
+              <img src={item.src} alt={item.title} />
+              <h1>{item.title}</h1>
+              <p>{item.description}</p>
+            </div>
+            <div className="description">
+              <a>saiba mais</a>
+            </div>
+          </section>
+        ) : null
+      )}
+    </section>
   );
 };
 
