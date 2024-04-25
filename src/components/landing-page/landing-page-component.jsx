8620172ab76212093136791b59e940eb52cb4c23
Joao Albuquerque <joaomarcos70@outlook.com.br>
2024-04-25 01:48:36 -0300

    feat creating glass eff

--- src/components/landing-page/landing-page-component.jsx
+++ src/components/landing-page/landing-page-component.jsx
@@ -6,6 +6,7 @@ import SkillComponent from "../skill-metric/skill";
 import Projetos from "../projetos/projetos";
 import me from "./assets/me.png";
 import IconComponent from "../../shared/icons/iconComponent";
+import GlassComponent from "../glass/glass.component";
 
 const LandingPageComponent = () => {
   const [isClickContato, setClickContato] = useState(true);
@@ -28,41 +29,43 @@ const LandingPageComponent = () => {
 
   return (
     <div className="landing-page">
-      <div className="header-icons">
-        <IconComponent icon="moon" size="32" color="#343E46"/>
-      </div>
-      <Header handleClick={(event) => handleClickBtn(event)} />
-      <div className="cols1">
-        <div className="slogan">
-          <h1>João Albuquerque</h1>
-          <p>
-            Desenvolvedor frontend experiente comprometido com soluções de alta
-            qualidade. Buscando desafios e colaboração em projetos inovadores.
-          </p>
+      <GlassComponent>
+        <div className="header-icons">
+          <IconComponent icon="moon" size="32" color="#343E46"/>
         </div>
-        <div className="skills">
-          <SkillComponent />
+        <Header handleClick={(event) => handleClickBtn(event)} />
+        <div className="cols1">
+          <div className="slogan">
+            <h1>João Albuquerque</h1>
+            <p>
+              Desenvolvedor frontend experiente comprometido com soluções de alta
+              qualidade. Buscando desafios e colaboração em projetos inovadores.
+            </p>
+          </div>
+          <div className="skills">
+            <SkillComponent />
+          </div>
+          {isClickContato ? (
+            <div className="contato" color="#ffffff">
+              <Contact />
+            </div>
+          ) : (
+            <div className="empty-div-contact"></div>
+          )}
         </div>
-        {isClickContato ? (
-          <div className="contato" color="#ffffff">
-            <Contact />
-          </div>
-        ) : (
-          <div className="empty-div-contact"></div>
-        )}
-      </div>
-      <div className="cols2">
-        <img src={me}></img>
-      </div>
-      <div className="cols3">
-        {isClickProjetos ? (
-          <div className="projeto">
-            <Projetos />
-          </div>
-        ) : (
-          <div></div>
-        )}
-      </div>
+        <div className="cols2">
+          {/* <img src={me}></img> */}
+        </div>
+        <div className="cols3">
+          {isClickProjetos ? (
+            <div className="projeto">
+              <Projetos />
+            </div>
+          ) : (
+            <div></div>
+          )}
+        </div>
+      </GlassComponent>
     </div>
   );
 };
