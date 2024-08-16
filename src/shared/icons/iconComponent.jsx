189755825df954a49c92bf58624dc1d9501a11da
Joao Albuquerque <joaomarcos70@outlook.com.br>
2024-08-16 01:23:54 -0300

    feat: SEO

--- src/shared/icons/iconComponent.jsx
+++ src/shared/icons/iconComponent.jsx
@@ -1,29 +1,28 @@
-import React from 'react';
-import * as feather from 'feather-icons';
-import './IconComponent.scss';
+import React from "react";
+import * as feather from "feather-icons";
 
 const IconComponent = (props) => {
-  const { icon, size, color, className } = props;
+	const { icon, size, color, className } = props;
 
-  const iconSize = size || 24;
-  const iconColor = color || 'black';
+	const iconSize = size || 24;
+	const iconColor = color || "black";
 
-  const iconStyle = {
-    width: `${iconSize}px`,
-    height: `${iconSize}px`,
-    color: iconColor,
-  };
+	const iconStyle = {
+		width: `${iconSize}px`,
+		height: `${iconSize}px`,
+		color: iconColor,
+	};
 
-  const renderedIcon = feather.icons[`${icon}`].toSvg({
-    ...iconStyle,
-    class: className,
-  });
+	const renderedIcon = feather.icons[`${icon}`].toSvg({
+		...iconStyle,
+		class: className,
+	});
 
-  return (
-    <div className="icon-container">
-      <div dangerouslySetInnerHTML={{ __html: renderedIcon }} />
-    </div>
-  );
-}
+	return (
+		<div className="icon-container">
+			<div dangerouslySetInnerHTML={{ __html: renderedIcon }} />
+		</div>
+	);
+};
 
 export default IconComponent;
